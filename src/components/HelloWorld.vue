768d3b94246231eb57e211469a51f59ab9094c07
GGbeng1 <15340019117@163.com>
2019-03-07 15:11:50 +0800

    :baby_chick: 增加展示

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <div class="tip">pc端请使用移动端模拟或使用移动端打开</div>
+    <div class="tipText">{{text}}</div>
+    <gesture-lock :containerWidth="400"
+                  :cycleRadius="30"
+                  @end="onEnd"
+                  :password="password"></gesture-lock>
+  </div>
+</template>
+
+<script>
+import GestureLock from '@/components/GestureLock';
+export default {
+  name: 'HelloWorld',
+  components: {
+    GestureLock
+  },
+  data () {
+    return {
+      title: "手势图案",
+      password: [],
+      text: '请设定手势'
+    }
+  },
+  methods: {
+    onEnd (data) {
+      if (this.password.length) {
+        if (this.password.join('') === data.join('')) {
+          this.text = '手势设定完成'
+          this.password = []
+        } else {
+          this.text = '两次手势设定不一致'
+          this.password = []
+        }
+      } else {
+        this.text = '请确认手势设定'
+        this.password = data
+      }
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.tip {
+  color: red;
+  margin-bottom: 20px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    https: false,
+    port: 9527,
+  },
+  assetsDir: 'docs'
+};
